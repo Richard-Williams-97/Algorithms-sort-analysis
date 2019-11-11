8131424dbc21042ca6ce9d6bb6d7c74a9dd65b08
Richard Williams <49793505+Richard-Williams-97@users.noreply.github.com>
2019-11-11 12:51:37 -0500

    This creates txt files with the times of each sort at size 1-1000000

--- test.java
+++ test.java
@@ -1,24 +1,77 @@
 import java.util.Random;
+import java.io.*;
 
 public class test
 {
    public static void main(String[] args)
    {  
-      
-      int[] arr;
-      Sort sorter=new Sort();
-      
-      for(int i=0;i<1000000;i=i*10)
+   
+      try
       {
-      arr=makeArray(i);
+         PrintWriter Bubble=new PrintWriter(new File("BubbleSort.txt"));
+         PrintWriter Insertion=new PrintWriter(new File("InsertionSort.txt"));
+         PrintWriter Merge=new PrintWriter(new File("MergeSort.txt"));
+         PrintWriter Quick=new PrintWriter(new File("QuickSort.txt"));
+         PrintWriter Selection=new PrintWriter(new File("SelectionSort.txt"));
+
+            
       
-      sorter.QuickSort(copy(arr), 0, arr.length-1);
-      sorter.MergeSort(copy(arr), 0, arr.length-1);
-      sorter.BubbleSort(copy(arr));
-      sorter.InsertionSort(copy(arr));
-      sorter.SelectionSort(copy(arr));
-  
+         int[] arr;
+         Sort sorter=new Sort();
+      
+         for(int i=1;i<=1000000;i=i*10)
+         {
+            arr=makeArray(i);
+            
+            long time;
+            
+            time=System.nanoTime();
+            sorter.BubbleSort(copy(arr));
+            time=System.nanoTime()-time;
+            
+             Bubble.write(time+"\n");
+            
+            time=System.nanoTime();
+            sorter.InsertionSort(copy(arr));
+            time=System.nanoTime()-time;
+            
+             Insertion.write(time+"\n");
+            
+            time=System.nanoTime();
+            sorter.MergeSort(copy(arr), 0, arr.length-1);
+            time=System.nanoTime()-time;
+            
+             Merge.write(time+"\n");
+            
+            time=System.nanoTime();
+            sorter.QuickSort(copy(arr), 0, arr.length-1);
+            time=System.nanoTime()-time;
+            
+             Quick.write(time+"\n");
+            
+            time=System.nanoTime();
+            sorter.SelectionSort(copy(arr));
+            time=System.nanoTime()-time;
+         
+             Selection.write(time+"\n");
+
+            System.out.println(i);
+         }
+      
+      
+      Bubble.close();
+      Insertion.close();
+      Merge.close();
+      Quick.close();
+      Selection.close();
+      
       }
+      catch(Exception e)
+      {
+         System.out.print("File not found");
+      }
+      
+   
    }
    
    //Copies array so that we can use one without affecting results
@@ -31,21 +84,21 @@ public class test
          array[i]=a[i];
       }
    
-   return array;
+      return array;
    }
    
    //makes array with random numbers of size s
    public static int[] makeArray(int s)
    {
    
-   int[] array=new int[s];
-   Random r=new Random();
+      int[] array=new int[s];
+      Random r=new Random();
    
       for(int i=0; i<s; i++)
       {
          array[i]=r.nextInt();
       }
       
-   return array;
+      return array;
    }
 }
